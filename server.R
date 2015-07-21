52eb20be51028a2f2684f79a5dfaf3b45e335aab
Steve Petersen <steve@maitapetersen.com>
2015-07-20 20:42:00 -0700

    final

--- server.R
+++ server.R
@@ -7,10 +7,24 @@ shinyServer(
       c1 <- input$c1
       c2 <- input$c2
       c3 <- input$c3
-      plot(sin(c1 * x) + sin(c2 * x) + sin(c3* x), xlab='t', ylab='Magnitude', col='lightblue',main='Components', ylim=c(-3, 3),  type='l')
-      text(63, 150, paste("component 1 = ", input$c1))
-      text(63, 150, paste("component 2 = ", input$c2))
-      text(63, 150, paste("component 3 = ", input$c3))
+      isum <- input$isum
+      icomp <- input$icomp
+      if (isum == FALSE && icomp == FALSE) {
+        plot(x, xlab='t', ylab='Magnitude', col='black',main='Components', ylim=c(-3, 3),  type='n')
+      } else {
+        if (isum == TRUE) {
+          plot(sin(c1 * x) + sin(c2 * x) + sin(c3* x), xlab='t', ylab='Magnitude', col='black',main='Components', ylim=c(-3, 3), lwd=3, type='l')
+          par(new=TRUE)
+        }
+        if (icomp == TRUE) {
+          plot(sin(c1 * x), xlab='t', ylab='Magnitude', col='red',main='Components', ylim=c(-3, 3),  type='l')
+          par(new=TRUE)
+          plot(sin(c2 * x), xlab='t', ylab='', col='green',main='', ylim=c(-3, 3),  type='l')
+          par(new=TRUE)
+          plot(sin(c3* x), xlab='t', ylab='', col='blue',main='', ylim=c(-3, 3),  type='l')
+          par(new=TRUE)
+        }
+      }
     })
     
   }
--- ui.R
+++ ui.R
@@ -1,11 +1,22 @@
 shinyUI(pageWithSidebar(
   headerPanel("Adding Sine Waves"),
   sidebarPanel(
-    sliderInput('c1', 'Component 1 Frequency',value = 1, min = 0, max = 10, step = 0.5),
-    sliderInput('c2', 'Component 2 Frequency',value = 1, min = 0, max = 10, step = 0.5),
-    sliderInput('c3', 'Component 3 Frequency',value = 1, min = 0, max = 10, step = 0.5)
-  ),
+    helpText("Set the Red, Green, and Blue component frequencies."),
+    sliderInput('c1', 'Red Component Frequency',value = 1, min = 0, max = 10, step = 0.5),
+    sliderInput('c2', 'Green Component Frequency',value = 2, min = 0, max = 10, step = 0.5),
+    sliderInput('c3', 'Blue Component Frequency',value = 3, min = 0, max = 10, step = 0.5),
+    helpText("To see the sum of all of the components, check the 'Show Sum' option."),
+    checkboxInput('isum', 'Show Sum (in black)', value = TRUE),
+    helpText("To see the individual components, check the 'Show Components' option."),
+    checkboxInput('icomp', 'Show Components', value = FALSE)
+    ),
   mainPanel(
+    helpText("The Adding Sine Waves application allows you to",
+             " visualize how sine waves of different frequencies combine.  Set one or more",
+             " component freqencies to zero to turn off that component, or from 1 to 10 to ",
+             " add that component's frequency to the sum.  Experiment with different values ",
+             " to explore constructive and destructive interference."
+    ),
     plotOutput('combination')
   )
 ))
